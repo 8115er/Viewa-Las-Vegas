9ad9da87951dcb14588f93c0e48a4119c5219a31
harmandhanoya <8115@htl.rennweg.at>
2022-04-11 10:49:08 +0200

    Core Part 2

--- index.php
+++ index.php
@@ -1,21 +1,65 @@
 <?php
 
-
 require_once 'vendor/autoload.php';
 
 use Oscorp\ViewaLasVegas\Hotel;
 
-$hotels[] = new Hotel("The Venetian", 'The Venetian Las Vegas embodies the spirit and excitement of Venice. An integrated Las Vegas resort. The Venetian offers all the romance and beauty of its namesake and more. USA Today calls it: "the most romantic hotel in Las Vegas, Nevada."', 5);
-$hotels[] = new Hotel("Trump International Hotel", 'Trump International Hotel Las Vegas is a sophisticated, non-smoking and non-gaming, luxury hotel situated high above the Las Vegas Strip. We are minutes away from the Las Vegas Convention Center and the best the city has to offer, while remaining removed enough for the perfect getaway.', 4);
-$hotels[] = new Hotel("Bellagio", "The hotel rooms and suites at the Bellagio offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Bellagio fountains right from your suite, your stay will be nothing short of unforgettable",4);
+$contents = file_get_contents("vegas.html");
+
+//replace Caesars_Palace
+$contents = str_replace(
+    '###caesars_palace_text###',
+    'This famous casino-hotel is in a sprawling Roman-style complex on the lively Las Vegas Strip. It
+            is 1.6 km from Interstate 15 and 22.5 km from Rio Secco Golf Club.
+            <br>
+            <br>
+            There are 11 restaurants in the hotel, including an upscale Japanese restaurant and a casual
+            grill restaurant. The casino offers classic gaming tables, poker and slot machines. Other
+            amenities include a seasonal outdoor pool, a gym, and a spa, as well as a nightclub and live
+            entertainment.',
+            $contents);
+$contents = str_replace('###caesars_palace_picture_main###', 'caesars_palace_main.jpg', $contents);
+$contents = str_replace('###caesars_palace_picture_1###', 'caesars_palace_1.jpg', $contents);
+$contents = str_replace('###caesars_palace_picture_2###', 'caesars_palace_2.jpg', $contents);
+
+//replace Bellagio
+$contents = str_replace(
+    '###bellagio_text###',
+    'This famous casino-hotel is in a sprawling Roman-style complex on the lively Las Vegas Strip. It
+            is 1.6 km from Interstate 15 and 22.5 km from Rio Secco Golf Club.
+            <br>
+            <br>
+            There are 11 restaurants in the hotel, including an upscale Japanese restaurant and a casual
+            grill restaurant. The casino offers classic gaming tables, poker and slot machines. Other
+            amenities include a seasonal outdoor pool, a gym, and a spa, as well as a nightclub and live
+            entertainment.',
+    $contents);
+$contents = str_replace('###bellagio_picture_main###', 'bellagio_main.jpg', $contents);
+$contents = str_replace('###bellagio_picture_1###', 'bellagio_1.jpg', $contents);
+$contents = str_replace('###bellagio_picture_2###', 'bellagio_2.jpg', $contents);
+
+//replace The Mirage
+$contents = str_replace(
+    '###the_mirage_text###',
+    'This legendary casino hotel overlooks the famous "erupting volcano" and is located on the Las
+            Vegas Strip next to the Caesars Palace hotel and across the street from Madame Tussauds Las
+            Vegas.
+            <br>
+            <br>
+            The chic rooms feature flat-screen TVs, free Wi-Fi, iPod docking stations and minibars. Suites
+            also have a separate living room, dining area and picture windows overlooking the Las Vegas
+            Strip. Apartments offer a private pool and butler service. Room service is available.
+            <br>
+            <br>
+            Amenities include a dolphin pool, an outdoor pool, an indoor saltwater aquarium, and several
+            bars and lounges. The hotel also has a convention center, spa, and shopping, as well as 14
+            restaurants, a popular casino, and live entertainment.',
+            $contents);
+$contents = str_replace('###the_mirage_picture_main###', 'the_mirage_main.jpg', $contents);
+$contents = str_replace('###the_mirage_picture_1###', 'the_mirage_1.jpg', $contents);
+$contents = str_replace('###the_mirage_picture_2###', 'the_mirage_2.jpg', $contents);
 
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
-$twig = new \Twig\Environment($loader);
+echo $contents;
 
-
-$template = $twig->load('index.html');
-
-echo $template->render(['a_variable' => 'Ich liebe Twig',
-                        'hotels' => $hotels]);
 
--- src/Hotel.php
+++ src/Hotel.php
@@ -4,20 +4,39 @@ namespace Oscorp\ViewaLasVegas;
 
 class Hotel
 {
-
     private $name;
     private $stars;
     private $description;
+    private $mainPicture;
+    private $secondaryPictures;
 
     /**
      * @param $name
      * @param $stars
      */
-    public function __construct($name, $description, $stars)
+    public function __construct($name, $description, $stars, $mainPicture, $secondaryPictures)
     {
         $this->name = $name;
         $this->description = $description;
         $this->stars = $stars;
+        $this->mainPicture = mainPicture;
+        $this->secondaryPictures = $secondaryPictures;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecondaryPictures()
+    {
+        return $this->secondaryPictures;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMainPicture()
+    {
+        return $this->mainPicture;
     }
 
     /**
