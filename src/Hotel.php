51f5e4c05ac786f7f7ebe64ce59df7b5a63528cf
Florexianer <8111@htl.rennweg.at>
2022-04-13 10:00:32 +0200

    Twig with final template

--- src/Hotel.php
+++ src/Hotel.php
@@ -8,17 +8,52 @@ class Hotel
     private $name;
     private $stars;
     private $description;
+    private $mainImg;
+    private $img1;
+    private $img2;
 
     /**
      * @param $name
      * @param $stars
+     * @param $description
+     * @param $img1
+     * @param $img2
      */
-    public function __construct($name, $description, $stars)
+    public function __construct($name, $stars, $description, $img1, $img2, $mainImg)
     {
         $this->name = $name;
-        $this->description = $description;
         $this->stars = $stars;
+        $this->description = $description;
+        $this->img1 = $img1;
+        $this->img2 = $img2;
+        $this->mainImg = $mainImg;
+
     }
+
+    /**
+     * @return string
+     */
+    public function getMainImg()
+    {
+        return $this->mainImg;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg1()
+    {
+        return $this->img1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg2()
+    {
+        return $this->img2;
+    }
+
 
     /**
      * @return mixed
